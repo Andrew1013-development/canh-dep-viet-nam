a25260819bb202cf14fcff92a009a12fbed94945
Andrew1013-development <viethuytran147@gmail.com>
2025-04-12 01:22:51 +0700

    preliminary cuisine page done

--- static/carousel.js
+++ static/carousel.js
@@ -5,8 +5,9 @@ items.forEach(item => {
     item.addEventListener("click", () => {
         const target_explainer = document.getElementById(item.getAttribute("data-target"));        
         if (active_explainer == target_explainer) return;
-        if (active_explainer != null) active_explainer.classList.remove("active");
-        target_explainer.classList.add("active");
+        if (active_explainer != null) active_explainer.classList.remove("active"); // hide active explainer
+        target_explainer.classList.add("active"); // enable target explainer
+        target_explainer.scrollIntoView({behavior: "smooth"}); // scroll into view
         active_explainer = target_explainer;
     });
 });
